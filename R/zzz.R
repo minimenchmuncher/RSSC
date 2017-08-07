8b64368f4d35b7539412128054ca9aac45a8621f
Tucker <227224@xcelenergy.com>
2017-08-07 13:16:33 -0600

    Added class template
    * Updated DESCRIPTION
    * added README
    * added class template functionality
    * added common types
    * provided pvwattsv5 class

---
+++ R/zzz.R
@@ -0,0 +1,21 @@
+#' @export
+get_common_types <- function(common_types_file = system.file("classes.yml",
+                                                             package = "RSSC")) {
+  yaml::yaml.load_file(common_types_file)
+}
+
+
+.onLoad <- function(libname, pkgname) {
+  # common types
+  if ("RSSC_common_types" %in% search()) detach(RSSC_common_types)
+  common_types <- get_common_types()
+  for (i in 1:length(common_types)) {
+    for (j in 1:length(common_types[[i]])) {
+      common_types[[i]][[j]]$default <- eval(parse(text = common_types[[i]][[j]]$default))
+    }
+  }
+
+  RSSC_common_types <- list2env(common_types)
+  attach(RSSC_common_types)
+  rm(i, j)
+}
---
+++ src/ssc_info.cpp
@@ -0,0 +1,22 @@
+#include <Rcpp.h>
+#include "sscapi.h"
+using namespace Rcpp;
+
+
+//'@export
+// [[Rcpp::export]]
+int R_ssc_version() {
+  int version = ssc_version();
+  return version;
+}
+
+// apparently, this works fine keeping as a string - R must be able to do that
+// conversion itself.
+//'@export
+// [[Rcpp::export]]
+std::string R_ssc_build_info() {
+  const char *build = ssc_build_info();
+  std::string build_s(build);
+  //String build_S = Rcpp::as<Rcpp::String>(build_s);
+  return build_s;
+}
